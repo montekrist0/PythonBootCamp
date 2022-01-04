bb133ec26b4180c7939a965696795e7480097e77
Igor <78171614+montekrist0@users.noreply.github.com>
2022-01-04 11:34:48 +0300

    Add JSON and exceptions

--- day29/main.py
+++ day29/main.py
@@ -1,6 +1,7 @@
 import tkinter.messagebox
 from tkinter import *
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # Password Generator Project
@@ -17,15 +18,11 @@ def gen_password():
     nr_letters = random.randint(8, 10)
     nr_symbols = random.randint(2, 4)
     nr_numbers = random.randint(2, 4)
-
     password_list = [random.choice(letters) for x in range(nr_letters)]
     password_list += [random.choice(symbols) for x in range(nr_symbols)]
     password_list += [random.choice(numbers) for x in range(nr_numbers)]
-
     random.shuffle(password_list)
-
     password = ''.join(password_list)
-
     pyperclip.copy(password)
     password_text.insert(0, password)
 
@@ -35,16 +32,46 @@ def save_data():
     site = website_text.get()
     email = email_text.get()
     password = password_text.get()
-    is_ok = tkinter.messagebox.askokcancel(title=site, message=f"Email:{email}\nPassword:{password}\n Proceed?")
+    new_data = {
+        site: {
+            "email": email,
+            "password": password
+        }
+    }
+    # is_ok = tkinter.messagebox.askokcancel(title=site, message=f"Email:{email}\nPassword:{password}\n Proceed?")
     if site == "" or email == "" or password == "":
         tkinter.messagebox.showinfo(title="Empty fields", message="Please don't left fields empty")
-    elif is_ok:
-        with open("data.txt", 'a') as file:
-            file.write(f"{site} | {email} | {password}\n")
+    else:
+        try:
+            with open("data.json", 'r') as file:
+                data = json.load(file)
+                data.update(new_data)
+        except FileNotFoundError:
+            with open("data.json", 'w') as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            with open("data.json", 'w') as file:
+                json.dump(data, file, indent=4)
+        finally:
             website_text.delete(0, END)
-            # email_text.delete(0, END)
             password_text.delete(0, END)
             tkinter.messagebox.showinfo(message="Password was successfully added to DB")
+
+
+# ---------------------------- SEARCH in DB ------------------------------- #
+def search():
+    try:
+        with open("data.json", 'r') as file:
+            data = json.load(file)
+            website = website_text.get()
+            tkinter.messagebox.showinfo(message=f"Data for '{website}' website:\n"
+                                                f"Email: {data[website]['email']}\n"
+                                                f"Password: {data[website]['password']}")
+
+    except FileNotFoundError:
+        tkinter.messagebox.showinfo(message="No data in Database yet")
+    except KeyError as key_error:
+        tkinter.messagebox.showinfo(message=f"There is no {key_error} website in Database")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -62,9 +89,11 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-website_text = Entry(width=38)
+website_text = Entry(width=21)
 website_text.focus()
-website_text.grid(row=1, column=1, columnspan=2)
+website_text.grid(row=1, column=1, columnspan=1)
+sch_button = Button(text="Search", width=13, command=search)
+sch_button.grid(row=1, column=2)
 
 email_text = Entry(width=38)
 email_text.insert(0, "user@mail.ru")
